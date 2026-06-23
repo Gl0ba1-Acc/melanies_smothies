733eb042bd9ba2603d62f34d724a3a0fd7ea5eda
Gl0ba1-Acc <golubetsy@gmail.com>
2026-06-23 17:37:09 +0300

    Change app title to 'My Parents New Healthy Diner

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f"Customize your Smoothie! :cup_with_straw:")
+st.title(f"My Parents New Healthy Diner:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
