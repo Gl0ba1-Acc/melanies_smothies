e48f87abc78dc907ad15c1d62d76bfd02c9124ad
Gl0ba1-Acc <golubetsy@gmail.com>
2026-06-24 10:26:25 +0300

    Update API call to include selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ ingredients_string = ''
 if ingredients_list:
     for fruit_chossen in ingredients_list:
         ingredients_string += fruit_chossen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st.subheader(fruit_chossen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chossen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
