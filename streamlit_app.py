16de1f397826a14c5005451e4be43306bc9adcbd
Gl0ba1-Acc <golubetsy@gmail.com>
2026-06-24 11:09:36 +0300

    Change fruit_options query and display dataframe
    Updated the query to select 'SEARCH-ON' instead of 'FRUIT_NAME' and enabled dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH-ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 # Add Multiselect
 ingredients_list = st.multiselect(
